bde6b66d1f4f1ec9d0e368fbef1c0957966e423d
Clint Hiles <clinthiles1992@gmail.com>
2024-08-20 10:21:47 -0700

    Queried first two stores and put them into an instance then updated store1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,5 +8,6 @@ Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens
 Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
+
 store_count = Store.count
 puts "There are #{store_count} stores"
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# store = Store.find_by(id: 1) can be done this way
+@store1 = Store.find(1)
+puts "store 1 is #{@store1.name}"
+
+@store2 = Store.find(2)
+puts "Store 2 is #{@store2.name}"
+
+@store1.update(name: "Burningby")
+puts "New title: #{@store1.name}"
