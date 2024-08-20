e755c619a0a26c51372d50922b82e3f0a7f7e651
Clint Hiles <clinthiles1992@gmail.com>
2024-08-20 11:20:26 -0700

    queried stores with avg revenue and stores that make over a milly

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_sum = Store.sum(:annual_revenue) # this will query and calculate the total revenue of all the stores combined
+puts "The total sum between stores is: #{@total_sum}"
+
+@avg_revenue = Store.average(:annual_revenue)
+puts "The average store revenue is: #{@avg_revenue.to_i}"
+
+@high_revenue = Store.where('annual_revenue >= ?', 1000000).count
+puts "The stores that generate over a milly and #{@high_revenue}"
