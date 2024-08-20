8c7da094ad24dfe9b3bca3b8d9ccf5a547b2c96e
Clint Hiles <clinthiles1992@gmail.com>
2024-08-20 10:27:04 -0700

    Destroyed store3

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -7,11 +7,11 @@ puts "----------"
 # Your code goes here ...
 
 # store = Store.find_by(id: 1) can be done this way
-@store1 = Store.find(1)
+@store1 = Store.find(1) # Find store by id put into and instance
 puts "store 1 is #{@store1.name}"
 
-@store2 = Store.find(2)
+@store2 = Store.find(2) # Same with store 2
 puts "Store 2 is #{@store2.name}"
 
-@store1.update(name: "Burningby")
+@store1.update(name: "Burningby")# Update the store name
 puts "New title: #{@store1.name}"
